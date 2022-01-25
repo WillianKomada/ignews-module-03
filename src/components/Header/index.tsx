cd618526149f45afc7bc8364e1a03ec395259f73
Willian Komada <willian10komada@hotmail.com>
2022-01-24 21:44:13 -0300

    tests Header SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
-import { ActiveLink } from '../ActiveLink';
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news"/>
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
@@ -14,7 +14,7 @@ export function Header() {
           {/* prefetch = pré carregar os dados de uma página */}
           <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
-          </ActiveLink>   
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,13 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
-import { signIn, signOut, useSession } from 'next-auth/client'
-import styles from './styles.module.scss';
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession()
+  const [session] = useSession();
 
   return session ? (
-    <button 
+    <button
       type="button"
       className={styles.signInButton}
       onClick={() => signOut()}
@@ -17,10 +17,10 @@ export function SignInButton() {
       <FiX color="737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button 
+    <button
       type="button"
       className={styles.signInButton}
-      onClick={() => signIn('github')}
+      onClick={() => signIn("github")}
     >
       <FaGithub color="#EBA417" />
       Sign in with Github
